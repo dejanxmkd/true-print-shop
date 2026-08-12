e2067e2f2d4367880f0302e8e8c4af369d09fdcc
Dejan Angjelkoski <53234041+dejanxmkd@users.noreply.github.com>
2026-08-12 21:39:25 +0200

    Set product title weight to 800

--- product.js
+++ product.js
@@ -17,7 +17,7 @@ const productName = find('#product-name');
 productName.textContent = details.displayName;
 productName.classList.toggle('title-short', details.displayName.length <= 20);
 productName.classList.toggle('title-long', details.displayName.length >= 34);
-productName.style.fontWeight = '600';
+productName.style.setProperty('font-weight', '800', 'important');
 productName.style.color = '#111111';
 find('#product-price').textContent = details.price;
 find('#product-brand').textContent = details.brand;
